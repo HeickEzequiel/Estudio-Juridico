5a45af0e659ec596e4e7b078c6837c58c6ac55b5
Ezequiel Heick <ezequielheick@gmail.com>
2024-07-04 18:25:05 -0300

    trabajando el home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,25 +3,26 @@ import Footer from "../footer/Footer"
 import Nav from "../nav/Nav"
 import { civil, familia, laboral } from "../../config/cloudinary"
 
+
 function Home() {
   return (
     <div>
         <Nav/>
-          <div className="relative border-2 border-black h-full w-full ">
-            <div className="text-center">
-              <p>Derecho de Familia y Sucesiones</p>
-              <AdvancedImage cldImg={familia}/>
-              <p className="mx-60">Divorcios - Responsabilidad Parental - Cuidado Personal de los niños - Compensaciones Económicas - Alimentos - Violencia Familiar - Filiación - Liquidación de Sociedades Conyugales - Sucesiones - Testamentos</p>
+          <div className="animate-fade-right">
+            <div className="relative border-double border-4 border-black shadow-2xl h-full w-75% left-48 mt-12 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-700 hover:bg-pasto">
+              <p className="text-center mt-4">Derecho de Familia y Sucesiones</p>
+              <div className="relative bottom-4 left-24"><AdvancedImage cldImg={familia}/></div>
+              <p className="absolute text-center top-16 left-8 mx-60">Divorcios - Responsabilidad Parental - Cuidado Personal de los niños - Compensaciones Económicas - Alimentos - Violencia Familiar - Filiación - Liquidación de Sociedades Conyugales - Sucesiones - Testamentos</p>
             </div>
-            <div className="text-center">
-              <p>Derecho Laboral</p>
-              <AdvancedImage cldImg={laboral}/>
-              <p className="mx-60">Despidos - Reclamos por Diferencias Salariales - Intercambio Telegráfico - Liquidaciones - Demandas Laborales - Acuerdos Espontáneos SECLO - Contratos de Trabajo - Accidentes de Trabajo - Abono a empresas - Seguimiento y Control.</p>
+            <div className="relative border-4 border-double border-black shadow-2xl h-full w-75% left-48 mt-12 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-700 hover:bg-pasto">
+              <p className="text-center mt-4">Derecho Laboral</p>
+              <div className="relative bottom-4 left-24"><AdvancedImage cldImg={laboral}/></div>
+              <p className="absolute text-center top-16 left-8 mx-60">Despidos - Reclamos por Diferencias Salariales - Intercambio Telegráfico - Liquidaciones - Demandas Laborales - Acuerdos Espontáneos SECLO - Contratos de Trabajo - Accidentes de Trabajo - Abono a empresas - Seguimiento y Control.</p>
             </div>
-            <div className="text-center">
-              <p>Derecho Civil - Daños</p>
-              <AdvancedImage cldImg={civil}/>
-              <p className="mx-60">Responsabilidad por Daños y Perjuicios - Accidentes de tránsito - Daños Resarcibles - Redacción y Revisión de Contratos. Seguimiento. Incumplimiento Contractual - Administración de Inmuebles. Asesoramiento y venta de inmuebles. Boletos de Compra Venta.</p>
+            <div className="relative border-4 border-double border-black shadow-2xl h-full w-75% left-48 mt-12 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-700 hover:bg-pasto">
+              <p className="text-center mt-4">Derecho Civil - Daños</p>
+              <div className="relative bottom-4 left-24"><AdvancedImage cldImg={civil}/></div>
+              <p className="absolute text-center top-16 left-8 mx-60">Responsabilidad por Daños y Perjuicios - Accidentes de tránsito - Daños Resarcibles - Redacción y Revisión de Contratos. Seguimiento. Incumplimiento Contractual - Administración de Inmuebles. Asesoramiento y venta de inmuebles. Boletos de Compra Venta.</p>
             </div>
           </div>
         <Footer/>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,29 +5,40 @@ export default {
   ],
   
   theme: {
-    colors:{
-      'pasto':'#a4b284',
-      'transparent': 'transparent',
-      'current': 'currentColor',
-      'white': '#ffffff',
-      'purple': '#3f3cbb',
-      'midnight': '#121063',
-      'metal': '#565584',
-      'tahiti': '#3ab7bf',
-      'silver': '#ecebff',
-      'bubble-gum': '#ff77e9',
-      'bermuda': '#78dcca',
-      'black':'#000000',
-    },
+    
     extend: {
       backgroundImage:{
         'imagen': "url('https://res.cloudinary.com/damoqjwmk/image/upload/v1719593614/Estudio%20Juridico/fondo.jpg')"
       },
+      fontFamily:{
+        cursiva:["Dancing Script"]
+      },
+
+      colors:{
+        'pasto':'#a4b284',
+        'transparent': 'transparent',
+        'current': 'currentColor',
+        'white': '#ffffff',
+        'purple': '#3f3cbb',
+        'midnight': '#121063',
+        'metal': '#565584',
+        'tahiti': '#3ab7bf',
+        'silver': '#ecebff',
+        'bubble-gum': '#ff77e9',
+        'bermuda': '#78dcca',
+        'black':'#000000',
+      },
+      spacing:{
+        '75%':'75%'
+      }
     },
+    
   },
   variants:{
     borderwidth: ['responsive', 'hover', 'focus']
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-animated')
+  ],
 }
 
